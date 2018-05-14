225ec4e12f949d78402f4d3626e575adac946fed
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2018-05-14 15:01:32 +0800

    update to download all data first ver.

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const cheerio = require('cheerio')
 const request = require('request')
 const http = require('http')
 const fs = require('fs')
-
-const wget = require('node-wget');
+// const wget = require('node-wget');
 const exec = require('child_process').exec;  
-const app = express()
+const app = express();
 const yearsUrl = "http://www.caa.gov.tw/big5/content/index.asp?sno=927"
 const allYearsDataFileName = "allYearsData.json"
 
@@ -21,60 +20,80 @@ app.use("/users", function(req, res, next){
 
 app.get('/download/', function(req, res) {
   var year = "2018";
-  var month = "01";
+  var month = "03";
 
   fs.stat(allYearsDataFileName, function(err, stat) {
     if(err == null) {
       console.log('File exists');
       var data = JSON.parse(fs.readFileSync(allYearsDataFileName));
-      // console.log(JSON.stringify(data));
-      var downloadLink = data[year]["Data"][month]["link"]
 
-      console.log(data[year]["Data"][month]["link"]);
-      // download(downloadLink, './test.xls');
-      download_file_wget(encodeURI(downloadLink), './downloads/test');
+      for(year in data){
+        console.log(year);
+        console.log("********");  
+        for(month in data[year]["Data"]){
+          console.log(month); 
+          var downloadLink = data[year]["Data"][month]["link"];
+          console.log(downloadLink);
+    
+          var finalFileType = downloadLink.substr(downloadLink.lastIndexOf('.') + 1);
+          var filename =  downloadLink.split('/').pop();
+          var dest = './downloads/' + year + month + '.' + finalFileType;
+          console.log('Downloading ' + filename);
+          downloadByFs2(encodeURI(downloadLink), dest, function(){console.log('Finished Downloading' + dest)});
+          
+        }
+  
+      }
 
       res.send('Get file done.');
+
+      // var downloadLink = data[year]["Data"][month]["link"];
+      // console.log(downloadLink);
+
+      // var finalFileType = downloadLink.substr(downloadLink.lastIndexOf('.') + 1);
+      // var filename =  downloadLink.split('/').pop();
+      // var dest = './downloads/' + year + month + '.' + finalFileType;
+      // console.log('Downloading ' + filename);
+      // downloadByFs2(encodeURI(downloadLink), dest, function(){console.log('Finished Downloading' + filename)});
+      // res.send('Get file done.');
+
+
     } else if(err.code == 'ENOENT') {
         // file does not exist
         fs.writeFile('log.txt', 'Some log\n');
     } else {
         console.log('Some other error: ', err.code);
     }
-});
-
+  });
 })
 
 
-var download_file_wget = function(file_url, DOWNLOAD_DIR) {    
-  wget({
-      url:  file_url,
-      dest: DOWNLOAD_DIR,      // destination path or path with filenname, default is ./
-      timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds
-    }, function(err, data) {        // data: { headers:{...}, filepath:'...' }
-    console.log('--- dry run data:');
-    console.log(data); // '/tmp/package.json'
-    }
-  );
-};  
+// const getAllLinkwithYearMonth()
 
 
 
 
 
-// ori
-// const download = function(url, dest, cb) {
-//   var file = fs.createWriteStream(dest);
-//   var request = http.get(url, function(response) {
-//     response.pipe(file);
-//     file.on('finish', function() {
-//       file.close(cb);  // close() is async, call cb after close completes.
-//     });
-//   }).on('error', function(err) { // Handle errors
-//     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-//     if (cb) cb(err.message);
-//   });
-// };
+// by fs
+const downloadByFs2 = function(url, dest, callback){
+  request.get(url)
+  .on('error', function(err) {console.log(err)} )
+  .pipe(fs.createWriteStream(dest))
+  .on('close', callback);
+};
+
+// by wget 
+// var download_file_wget = function(file_url, DOWNLOAD_DIR) {    
+//   wget({
+//       url:  file_url,
+//       dest: DOWNLOAD_DIR,      // destination path or path with filenname, default is ./
+//       timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds
+//     }, function(err, data) {        // data: { headers:{...}, filepath:'...' }
+//     console.log('--- dry run data:');
+//     console.log(data); // '/tmp/package.json'
+//     }
+//   );
+// };  
 
 app.get('/scrap', function(req, res) {
   var allYearsJson = {};  
@@ -86,8 +105,6 @@ app.get('/scrap', function(req, res) {
     console.log(allYearsJson);
 
     fs.writeFileSync(allYearsDataFileName, allYearsJson);
-
-
     res.send(allYearsJson);
     // res.send('Scrape done!!');
     // res.send('OK!')
@@ -109,13 +126,10 @@ const setAirlinesLink = function () {
         method: "GET"
       }, function (error, response, body) {
         if (error || !body) {
-          console.log("2");
           console.log(error);
           return;
         }
-        console.log("3");
         const $ = cheerio.load(body, {decodeEntities: false}); // 載入 body     
-        console.log("4");
         var yearsLink = myScript.getYearsLink($);
         // get all promise object
         var yearsStrArr = Object.keys(yearsLink);
